0dca486d8acde0167a3fb97236332f34b287e36c
spigioe <spiegelhalter.dominik@stud.uni-obuda.hu>
2026-08-24 01:52:29 +0200

    feat: SLA due date calculation on ticket create, breach checker background job

--- backend/SupportPortal/obj/Debug/net9.0/SupportPortal.AssemblyInfo.cs
+++ backend/SupportPortal/obj/Debug/net9.0/SupportPortal.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SupportPortal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3608274c81f25341764d0d60c062a6b3650cc78d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b60428aef048a2d9497f1b75442881e6473332b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("SupportPortal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SupportPortal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
